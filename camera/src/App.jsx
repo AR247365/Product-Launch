5cd9890246e47a3e995e87832182def94555e719
AR247365 <lonewolf22081998@gmail.com>
2024-03-22 02:23:53 +0530

    Toggle orbit control

--- camera/src/App.jsx
+++ camera/src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 import gsap from "gsap"
 import ScrollTrigger from "gsap/dist/ScrollTrigger"
 import { Environment, Loader, OrbitControls } from "@react-three/drei"
@@ -9,10 +10,18 @@ import { Suspense } from "react"
 gsap.registerPlugin(ScrollTrigger)
 
 export default function App() {
+  const [enableZoom, setEnableZoom] = useState(false)
+  const [enableRotate, setEnableRotate] = useState(false)
+
+  const toggleZoomAndRotate = () => {
+    setEnableZoom((prev) => !prev)
+    setEnableRotate((prev) => !prev)
+  }
+
   return (
     <>
       <main className="">
-        <div className="h-screen w-full fixed top-0 z-10  ">
+        <div className="h-screen w-full fixed top-0 z-10 ">
           <Suspense fallback={<Loader />}>
             <Canvas
               camera={{
@@ -22,10 +31,10 @@ export default function App() {
                 position: [0, 3, 6],
               }}
             >
+              {/* Pass state variables as props to OrbitControls */}
               <OrbitControls
-                makeDefault
-                enableZoom={false}
-                enableRotate={false}
+                enableZoom={enableZoom}
+                enableRotate={enableRotate}
                 enablePan={false}
               />
 
@@ -35,8 +44,51 @@ export default function App() {
           </Suspense>
         </div>
 
-        <Overlay />
+        <Overlay toggleZoomAndRotate={toggleZoomAndRotate} />
       </main>
     </>
   )
 }
+
+// import gsap from "gsap"
+// import ScrollTrigger from "gsap/dist/ScrollTrigger"
+// import { Environment, Loader, OrbitControls } from "@react-three/drei"
+// import Overlay from "./Overlay"
+// import Model from "./Model"
+// import { Canvas } from "@react-three/fiber"
+// import { Suspense } from "react"
+
+// gsap.registerPlugin(ScrollTrigger)
+
+// export default function App() {
+//   return (
+//     <>
+//       <main className="">
+//         <div className="h-screen w-full fixed top-0 z-10  ">
+//           <Suspense fallback={<Loader />}>
+//             <Canvas
+//               camera={{
+//                 fov: 45,
+//                 near: 0.1,
+//                 far: 200,
+//                 position: [0, 3, 6],
+//               }}
+//             >
+//               <OrbitControls
+//                 makeDefault
+//                 enableZoom={false}
+//                 enableRotate={false}
+//                 enablePan={false}
+//               />
+
+//               <Environment files="./src/assets/environment.hdr" />
+//               <Model />
+//             </Canvas>
+//           </Suspense>
+//         </div>
+
+//         <Overlay />
+//       </main>
+//     </>
+//   )
+// }
--- camera/src/Overlay.jsx
+++ camera/src/Overlay.jsx
@@ -1,7 +1,15 @@
-export default function Overlay() {
+import { useState } from "react"
+
+export default function Overlay({ toggleZoomAndRotate }) {
+  const [isViewing3D, setIsViewing3D] = useState(true)
+  const handleClick = () => {
+    toggleZoomAndRotate()
+    setIsViewing3D((prev) => !prev)
+  }
+
   return (
     <>
-      <div className="px-14 py-12">
+      <div className="px-14 py-10">
         {/* HERO SECTION */}
 
         <section className="h-screen">
@@ -49,7 +57,7 @@ export default function Overlay() {
           <div className="flex flex-row">
             <div className="flex flex-col basis-5/12 ">
               <h1 className="text-7xl font-semibold text-red-700">Power</h1>
-              <p className="font-normal text-lg tracking-tighter pt-2 pl-2">
+              <p className="font-normal text-lg tracking-tighter ">
                 The easy-to-carry EOS R10 packs advanced features into a
                 lightweight, compact design. Pair with a Canon RF-S/RF lens for
                 a high-performance setup that fits easily and comfortably in
@@ -70,7 +78,7 @@ export default function Overlay() {
               <h1 className="text-7xl font-semibold m-auto ">
                 Smart <span className="text-red-700">autofocus</span>
               </h1>
-              <p className="font-normal text-lg tracking-tighter pt-2 pl-2 m-auto text-center">
+              <p className="font-normal text-lg tracking-tighter  m-auto text-center">
                 Canon's Dual Pixel CMOS AF technology with people, animal, and
                 vehicle subject detection lets you keep your eye on the action
                 while it keeps your subject in crystal clear focus.
@@ -81,15 +89,121 @@ export default function Overlay() {
         </section>
 
         {/* GALLERY SECTION */}
-        <section id="gallery" className="h-screen">
-          <div className="h-screen flex flex-row justify-evenly pb-16">
+        <section id="gallery" className="h-screen relative">
+          <div className="h-screen flex flex-row justify-evenly ">
             <div className="basis-1/2"></div>
-            <h1 className="text-7xl font-semibold  text-center mx-auto my-auto  ">
-              View <span className="text-red-700">3D</span>
-            </h1>
+
+            <div
+              className={`z-50 flex flex-col ${
+                isViewing3D ? "" : "absolute bottom-0"
+              }`}
+            >
+              <button className="my-auto" onClick={handleClick}>
+                <h1 className="text-7xl font-semibold  text-center mx-auto my-auto">
+                  {isViewing3D ? "View " : "Exit "}{" "}
+                  <span className="text-red-700">3D</span>
+                </h1>
+              </button>
+            </div>
           </div>
         </section>
       </div>
     </>
   )
 }
+
+// export default function Overlay() {
+//   return (
+//     <>
+//       <div className="px-14 py-10">
+//         {/* HERO SECTION */}
+
+//         <section className="h-screen">
+//           <div className="flex flex-row">
+//             <div className="flex flex-col basis-5/12 ">
+//               <img
+//                 src="./src/assets/logo.png"
+//                 alt="logo"
+//                 width="150vw"
+//                 height="auto"
+//               />
+//               <h1 className="text-9xl font-semibold ">Pro</h1>
+//               <p className="font-normal text-lg tracking-tighter pt-2 pl-2">
+//                 Discover our most advanced camera and lens series yet: blazing
+//                 fast AF, incredible low light performance, superb image
+//                 stabilization, sharp image quality, and so much more
+//               </p>
+//             </div>
+//             <div></div>
+//           </div>
+//         </section>
+
+//         {/* PERFORMANCE SECTION */}
+//         <section id="perf-sec" className=" h-screen">
+//           <div className="flex flex-row">
+//             <div className="basis-2/3"> </div>
+//             <div className="flex flex-col basis-1/3 ">
+//               <h1 className="text-7xl font-semibold text-red-700">
+//                 Performance
+//               </h1>
+//               <p className="font-normal text-lg tracking-tighter pt-7">
+//                 The EOS R10 is perfect for content creators looking to take
+//                 their creativity to the next level. Featuring a high-speed
+//                 shooting 15 FPS mechanical shutter1,2, a 24.2 Megapixel CMOS
+//                 (APS-C) sensor, and lightning fast autofocus, the EOS R10 camera
+//                 brings some of the best features from the growing EOS R Series
+//                 to a sleek, lightweight design
+//               </p>
+//             </div>
+//           </div>
+//         </section>
+
+//         {/* POWER SECTION */}
+//         <section id="pow-sec" className="h-screen">
+//           <div className="flex flex-row">
+//             <div className="flex flex-col basis-5/12 ">
+//               <h1 className="text-7xl font-semibold text-red-700">Power</h1>
+//               <p className="font-normal text-lg tracking-tighter pt-2 pl-2">
+//                 The easy-to-carry EOS R10 packs advanced features into a
+//                 lightweight, compact design. Pair with a Canon RF-S/RF lens for
+//                 a high-performance setup that fits easily and comfortably in
+//                 your hand.
+//               </p>
+//             </div>
+//             <div className="basis-5/12"></div>
+//             <div className="basis-2/12">
+//               <img src="./src/assets/features.png" alt="features" />
+//             </div>
+//           </div>
+//         </section>
+
+//         {/* SMART SECTION */}
+//         <section id="smart-sec" className="h-screen">
+//           <div className="flex flex-col ">
+//             <div className="basis-1/3 flex flex-col ">
+//               <h1 className="text-7xl font-semibold m-auto ">
+//                 Smart <span className="text-red-700">autofocus</span>
+//               </h1>
+//               <p className="font-normal text-lg tracking-tighter pt-2 pl-2 m-auto text-center">
+//                 Canon's Dual Pixel CMOS AF technology with people, animal, and
+//                 vehicle subject detection lets you keep your eye on the action
+//                 while it keeps your subject in crystal clear focus.
+//               </p>
+//             </div>
+//             <div className="basis-2/3"></div>
+//           </div>
+//         </section>
+
+//         {/* GALLERY SECTION */}
+//         <section id="gallery" className="h-screen">
+//           <div className="h-screen flex flex-row justify-evenly ">
+//             <div className="basis-1/2"></div>
+//             <h1 className="text-7xl font-semibold  text-center mx-auto my-auto  ">
+//               View <span className="text-red-700">3D</span>
+//             </h1>
+//           </div>
+//         </section>
+//       </div>
+//     </>
+//   )
+// }
